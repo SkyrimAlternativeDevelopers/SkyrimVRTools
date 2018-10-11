2f5a53adcdfa763e08c6036d5caa472b08a117cb
Jonathan Shieh <jonathan.shieh@gmail.com>
2018-10-11 14:50:28 +0800

    Now able to register and unregister input handling callbacks

--- src/ScaleformVR.cpp
+++ src/ScaleformVR.cpp
@@ -3,9 +3,23 @@
 #include "skse64/ScaleformMovie.h"
 #include "skse64/Hooks_Scaleform.h"
 #include "skse64/ScaleformCallbacks.h"
+#include <list>
 
 namespace ScaleformVR
 {
+	struct ScaleformCallback {
+		const GFxValue	* object;
+		const char		* methodName;
+
+		bool operator==(const ScaleformCallback& rhs)
+		{
+			return (this->object && rhs.object) && (0 == strcmp(this->methodName, rhs.methodName));
+		}
+	};
+
+	typedef std::list <ScaleformCallback> ScaleformCallbackList;
+	static ScaleformCallbackList g_scaleformInputHandlers;
+
 	class VRInputScaleform_ShutoffButtonEventsToGame : public GFxFunctionHandler
 	{
 	public:
@@ -15,7 +29,7 @@ namespace ScaleformVR
 
 			ASSERT(args->numArgs >= 1);
 
-			bool		shutoff = args->args[0].GetBool();
+			bool shutoff = args->args[0].GetBool();
 
 			_MESSAGE("ShutoffButtonEventsToGame: %d", shutoff);
 		}
@@ -26,7 +40,28 @@ namespace ScaleformVR
 	public:
 		virtual void	Invoke(Args * args)
 		{
-			_MESSAGE("RegisterInputHandler dll fn called!");
+			//_MESSAGE("RegisterInputHandler dll fn called!");
+
+			ASSERT(args->numArgs == 2);
+			ASSERT(args->args[0].GetType() == GFxValue::kType_DisplayObject);
+			ASSERT(args->args[1].GetType() == GFxValue::kType_String);
+
+			ScaleformCallback callback;
+			callback.object = &args->args[0];
+			callback.methodName = args->args[1].GetString();
+
+			// If the same callback has already been registered, do not add it again.
+			// This will cause the callback to triggered twice when input events are sent.
+			for (ScaleformCallbackList::iterator iter = g_scaleformInputHandlers.begin(); iter != g_scaleformInputHandlers.end(); ++iter)
+			{
+				if (*iter == callback) {
+					_MESSAGE("Duplicate callback found, aborting register...");
+					return;
+				}
+			}
+
+			_MESSAGE("Registering: %s", callback.methodName);
+			g_scaleformInputHandlers.push_back(callback);
 		}
 	};
 
@@ -35,12 +70,22 @@ namespace ScaleformVR
 	public:
 		virtual void	Invoke(Args * args)
 		{
-			_MESSAGE("VRInputScaleform_UnregisterInputHandler dll fn called!");
+			//_MESSAGE("VRInputScaleform_UnregisterInputHandler dll fn called!");
+			ASSERT(args->numArgs == 2);
+			ASSERT(args->args[0].GetType() == GFxValue::kType_DisplayObject);
+			ASSERT(args->args[1].GetType() == GFxValue::kType_String);
+
+			ScaleformCallback callback;
+			callback.object = &args->args[0];
+			callback.methodName = args->args[1].GetString();
+
+			_MESSAGE("Unregistering: %s", callback.methodName);
+			g_scaleformInputHandlers.remove(callback);
 		}
 	};
 
 	bool RegisterFuncs(GFxMovieView * view, GFxValue * plugin) {
-		_MESSAGE("Registering scaleform functions");
+		//_MESSAGE("Registering scaleform functions");
 
 		RegisterFunction<VRInputScaleform_ShutoffButtonEventsToGame>(plugin, view, "ShutoffButtonEventsToGame");
 		RegisterFunction<VRInputScaleform_RegisterInputHandler>(plugin, view, "RegisterInputHandler");
